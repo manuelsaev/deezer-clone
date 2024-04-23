276cf17107283a3358525642c8d307ba5fc652c6
Manuel Sotelo <manuel@evolbit.net>
2024-04-22 23:15:37 -0500

    Generando los migrations para: Song, Album y Artist

---
+++ database/migrations/2024_04_23_041044_create_songs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('album_id')->unsigned()->nullable();
+            $table->foreign('album_id')->references('id')->on('albums');
+            $table->string('title');
+            $table->string('link', 500);
+            $table->string('preview');
+            $table->integer('duration');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('songs');
+    }
+};
